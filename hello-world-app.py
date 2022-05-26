6783bc2b15793f7a8f8ac644a8d122ba24ab5602
ggorit06 <99786874+ggorit06@users.noreply.github.com>
2022-05-25 23:39:32 -0500

    Update hello-world-app.py

--- hello-world-app.py
+++ hello-world-app.py
@@ -6,20 +6,19 @@ app = Flask(__name__)
 def head():
     return "<h1>Hello World!</h1>"
 
-@app.route ("/second")
+
+@app.route("/second")
 def second():
     return "This is my second page"
 
 @app.route("/third/subthird")
 def third():
-    return "<h2>This is the subpath of the third page</h2>"
+    return "<h2>This is the subpath of third page</h2>"
 
 @app.route("/forth/<string:id>")
-def forth():
-    return "<h2>f'Id of this page is {id}'</h2>"
+def forth(id):
+    return f'Id of this page is {id}'
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
